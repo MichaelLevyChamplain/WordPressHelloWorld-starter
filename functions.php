318e637be3026d807ed54b22d3351fb4eb0b18cd
michaelslevy <u41745915@cbi-bs1.server.lan>
2020-03-22 15:41:21 -0400

    Feat: Initializing Repository

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+
+/*https://developer.wordpress.org/reference/functions/wp_enqueue_style/ */
+
+/* add a title tag */
+/* https://codex.wordpress.org/Title_Tag */
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+	<!--insert wp_head() -->
+	<!-- https://developer.wordpress.org/reference/functions/wp_head/ -->
+	</head>
+
+	<!--insert body_class(); -->
+	<!--https://developer.wordpress.org/reference/functions/body_class/-->
+	<body>
+
+	<!--insert wp_nav_menu(); -->
+	<!--https://developer.wordpress.org/reference/functions/wp_nav_menu/ -->
+	<header></header>
+
+	<div id="content">
+		<h1>Hello World</h1>
+		<!--Insert Wordpress loop-->
+		<!-- https://developer.wordpress.org/themes/basics/the-loop/ -->
+	</div>
+
+	<!--insert wp_footer(); -->
+	<!--https://developer.wordpress.org/reference/functions/wp_footer/-->
+	</body>
+
+</html>
